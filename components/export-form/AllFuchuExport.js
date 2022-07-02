9e2abe77771dc747aae4288fcd1c6ae264f78bcf
59417 <sun313131@gmail.com>
2022-07-02 19:00:06 +0800

    Modify export tables including company's name

--- components/export-form/AllFuchuExport.js
+++ components/export-form/AllFuchuExport.js
@@ -1,5 +1,6 @@
 export default function AllFuchuExport() {
     return (
-        <div><h1 style={{fontSize: '3rem', textAlign: 'center', color: 'green'}}>富櫥總估價單</h1></div>
+        // <div><h1 style={{fontSize: '3rem', textAlign: 'center', color: 'green'}}>富櫥總估價單</h1></div>
+        <div><h1 style={{fontSize: '3rem', textAlign: 'center', color: 'green'}}>總估價單B</h1></div>
     )
 };
--- components/export-form/OneFuchuExport.js
+++ components/export-form/OneFuchuExport.js
@@ -1,5 +1,6 @@
 export default function OneFuchuExport() {
     return (
-        <div><h1 style={{fontSize: '3rem', textAlign: 'center', color: 'blue'}}>富櫥估價單</h1></div>
+        // <div><h1 style={{fontSize: '3rem', textAlign: 'center', color: 'blue'}}>富櫥估價單</h1></div>
+        <div><h1 style={{fontSize: '3rem', textAlign: 'center', color: 'blue'}}>估價單B</h1></div>
     )
 };
